750d355594665a0880a784d7e93e1010a4510e52
Michelle La <michelle.la@nytimes.com>
2024-08-05 03:02:06 -0400

    Create game handlers

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module multiplayer-tic-tac-toe
+
+go 1.22.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
+	"sync"
+)
+
+var (
+	mu sync.Mutex
 )
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/create", createGameHandler).Methods("POST")
+	r.HandleFunc("/play/{id}", playGameHandler).Methods("GET")
+
 	// register handler for root URL "/"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("test")
@@ -16,4 +28,30 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server failed:", err)
 	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Tic-Tac-Toe!")
+}
+
+func createGameHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	id := 0
+	// instantiate new game
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]int{"gameID": id})
+}
+
+func playGameHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// gameID := vars["id"]
+
+	mu.Lock()
+	// check if game exists with that gameID and retrieve
+	mu.Unlock()
+
+	// definition and logic to make moves
 }
